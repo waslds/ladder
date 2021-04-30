c656f59bad870d77e603195835f8f012cbaed6c2
lds <lds@DESKTOP-S04JOM9>
2021-04-30 16:05:46 +0900

    사다리타기구현 2-1

--- ladder/src/ladder/Ladder.java
+++ ladder/src/ladder/Ladder.java
@@ -2,4 +2,30 @@ package ladder;
 
 public class Ladder {
 
+	int[] row;
+	
+	public Ladder(int noOfPerson) {
+		row = new int[noOfPerson];
+	}
+	
+	public void drawLine(int startPosition) {
+		row[startPosition] = 1;
+		row[startPosition + 1] = 1;
+	}
+
+	public int run(int nthOfPerson) {
+		if (row[nthOfPerson] == 0) {
+			return nthOfPerson;
+		}
+		
+		if (nthOfPerson - 1 >= 0) {
+			int leftValue = row[nthOfPerson - 1];
+			if (leftValue == 1) {
+				return nthOfPerson - 1;
+			}
+		}
+		
+		return nthOfPerson + 1;
+	}
+	
 }
--- ladder/test/ladder/LadderTest.java
+++ ladder/test/ladder/LadderTest.java
@@ -7,8 +7,46 @@ import org.junit.Test;
 public class LadderTest {
 
 	@Test
-	public void test() {
+	public void testRunWhenNoLine() throws Exception {
+		Ladder ladder = new Ladder(3);
+		int target = ladder.run(0);
+		assertEquals(0, target);
 		
+		target = ladder.run(2);
+		assertEquals(2, target);
 	}
 
+	@Test
+	public void testRunWhenLineLeft() throws Exception {
+		// 0 1 1
+		Ladder ladder = new Ladder(3);
+		ladder.drawLine(1);
+		
+		int target = ladder.run(2);
+		assertEquals(1, target);
+		
+		// 1 1 0
+		ladder = new Ladder(3);
+		ladder.drawLine(0);
+		
+		ladder.run(0);
+	}
+	
+	@Test
+	public void testRunWhenLineRight() throws Exception {
+		// 0 1 1
+		Ladder ladder = new Ladder(3);
+		ladder.drawLine(1);
+		
+		int target = ladder.run(1);
+		assertEquals(2, target);
+		
+		// 0 1 1
+		ladder = new Ladder(3);
+		ladder.drawLine(1);
+		
+		target = ladder.run(2);
+		assertEquals(1, target);
+	}
+	
 }
